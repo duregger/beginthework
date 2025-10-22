477fbfca63460d516004ffd66ac9389dc31c3ae2
duregger <sam@samduregger.com>
2025-10-22 10:36:22 -0500

    Fix CSS loading in production - import CSS files in React components

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,15 @@ import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
 import './index.css'
+import './assets/css/bootstrap.min.css'
+import './assets/css/owl.carousel.min.css'
+import './assets/css/animate.min.css'
+import './assets/css/magnific-popup.css'
+import './assets/css/fontawesome-all.min.css'
+import './assets/css/slick.css'
+import './assets/css/default.css'
+import './assets/css/style.css'
+import './assets/css/responsive.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
